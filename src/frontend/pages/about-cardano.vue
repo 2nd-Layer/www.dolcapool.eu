f7512e9a35aafaa9a05a38b95b05f1944d73c24b
Mark Stopka <mark.stopka@perlur.cloud>
2020-10-10 00:47:30 +0200

    Update About Cardano page (#53)
    * Add link to Ouroboros Clepsydra
    * Update Ouroboros links to IACR Abstract
    * Add content
    * Add content and CTA banner

--- src/frontend/pages/about-cardano.vue
+++ src/frontend/pages/about-cardano.vue
@@ -12,7 +12,8 @@
         </h2>
         <p class="content pl-2">
           Cardano is a 3<sup>rd</sup> generation blockchain, project started in 2015 and debuted publicly in late 2017. Cardano uses proof-of-stake consensus and relies heavily on peer-reviewed science. Cardano uses unique, provably secure Proof-of-stake consensus algorithm from the Ouroboros family.
-        </p>        
+        </p>
+        <p>In addition similarly to Bitcoin, it usses UTXO accounting model, Extended UTXO specifically to enable <strong class="has-text-white">Proof-of-Stake</strong> and <strong class="has-text-white">Smart Contracts</strong>. Compared to <strong class="has-text-white">Account based accounting model<strong class="has-text-white">, it provides greater opportunities for scaling than <strong class="has-text-white">Account based accounting model<strong class="has-text-white">.
       </div>
     </div>
     <div class="columns has-text-left">
@@ -21,14 +22,24 @@
           About Ouroboros Proof-of-Stake
         </h3>
         <p class="content pl-2">
-          Provably secure Proof-of-Stake consensus protocol Ouroboros was first introduced in the paper <a href="https://eprint.iacr.org/2016/889.pdf">Ouroboros: A Provably Secure Proof-of-Stake Blockchain Protocol</a> now simply referred to as <strong class="has-text-white">Ouroboros Classic</strong>, since then, many iterations of Ouroboros protocol have been published improving different properties and making certain trade-offs... Other papers from the Ouroboros series include:
+          Provably secure Proof-of-Stake consensus protocol Ouroboros was first introduced in the paper <a href="https://eprint.iacr.org/2016/889">Ouroboros: A Provably Secure Proof-of-Stake Blockchain Protocol</a> now simply referred to as <strong class="has-text-white">Ouroboros Classic</strong>, since then, many iterations of Ouroboros protocol have been published improving different properties and making certain trade-offs... Other papers from the Ouroboros series include:
           <ul>
-            <li><a href="https://eprint.iacr.org/2017/573.pdf">Ouroboros Praos: An adaptively-secure, semi-synchronous proof-of-stake blockchain</a></li>
-            <li><a href="https://eprint.iacr.org/2018/378.pdf">Ouroboros Genesis: Composable Proof-of-Stake Blockchains with Dynamic Availability</a></li>
-            <li><a href="https://eprint.iacr.org/2018/1132.pdf">Ouroboros Crypsinous: Privacy-Preserving Proof-of-Stake</a></li>
-            <li><a href="https://eprint.iacr.org/2019/838.pdf">Ouroboros Chronos: Permissionless Clock Synchronization via Proof-of-Stake</a></li>
+            <li><a href="https://eprint.iacr.org/2017/573">Ouroboros Praos: An adaptively-secure, semi-synchronous proof-of-stake blockchain</a></li>
+            <li><a href="https://eprint.iacr.org/2018/378">Ouroboros Genesis: Composable Proof-of-Stake Blockchains with Dynamic Availability</a></li>
+            <li><a href="https://eprint.iacr.org/2018/1132">Ouroboros Crypsinous: Privacy-Preserving Proof-of-Stake</a></li>
+            <li><a href="https://eprint.iacr.org/2019/838">Ouroboros Chronos: Permissionless Clock Synchronization via Proof-of-Stake</a></li>
+            <li><a href="https://eprint.iacr.org/2019/942">Ouroboros Clepsydra: Ouroboros Praos in the Universally Composable Relative Time Model</a></li>
           </ul>
-        </p>    
+        </p>
+        <p class="content pl-2">Current Csardano Shelley mainnet operates using <a href="https://eprint.iacr.org/2017/573">Ouroboros Praos</a> Proof-of-Stake consensus mechanism. Two major values influencing the network topology are <code>k</code> which controls the amount of stakepools the system should converge to, that is currently set to <strong class="has-text-white">150</strong>, based on Cardano Shelley Incentivized Testnet, this should bring effectibve amount of pools to <i>250</i> - <i>300</a>. Another such important parameter is <code>a0</code> which drives influence of <strong class="has-text-white">owners stake</strong> on rewards for himself and his delegates. With current setting of <i>0.3</i>. With these settings, running a stake pool becamse a commoditized market with thin margins and low-added value. We strive to be high added value Cardano Stake Poool.</p>
+        <!-- CTA -->
+        <p>
+          <nuxt-link to="/begin-staking-now/" class="button k-button k-primary raised has-gradient is-fat is-bold">
+            <span class="text">Leanr More about DOLCA Stake Pool</span>
+            <span class="front-gradient"></span>
+          </nuxt-link>
+        </p>
+      </div>
       </div>
       <div class="column is-narrow">
         <div class="token-image mb-40" style="filter: invert(100%);">
