8c7d732e2dfa18af699419e4dbb2279b0291aedc
Mark Stopka <mark.stopka@perlur.cloud>
2020-10-17 03:44:06 +0200

    Update content

--- src/frontend/pages/about-cardano.vue
+++ src/frontend/pages/about-cardano.vue
@@ -151,8 +151,15 @@
           <h3 class="subtitle is-4 is-light is-thin pr-2">
             Extended UTXO Accounting Model
           </h3>
-          <p class="content pl-2" />
-          <p class="content pl-2" />
+          <p class="content pl-2">
+            As you may already know, <strong>Bitcoin</strong> uses UTXO or Unspent Transaction Output accoutning style, in contrast with <strong>Ethereum</strong> or <strong>Tezos</strong> which use account style accounting models.
+          </p>
+          <p class="content pl-2">
+            As it often is, both accounting models offer some advantages and some disadvantages, major advantage of UTXO accounting model is that due to lack of global account state (accounts are emptied during transaction which enables the wallet node to purge it and save memory), smart-contract functionality is easier to implement using account style accounting model, that is however more complicated to scale due to the need to maintain global state of all accounts.
+          </p>
+           <p class="content pl-2">
+            For these reasons, <a href="https://www.iohk.io/">Input Output Global</a> decided to invent a new model <a href="https://iohk.io/en/research/library/papers/the-extended-utxo-model/">called Extended UTXO</a> which combines the benefits of both Unspent Transaction Outputs accounting and account style accounting. This unique accounting model enables <a href="#hydra">Layer 2 scaling tehcnology like lated described Hydra</a>.
+          </p>         
           <!-- CTA -->
           <p>
             <nuxt-link
@@ -219,7 +226,9 @@
       <div class="columns has-text-left">
         <div class="column">
           <h3 class="subtitle is-4 is-light is-thin pr-2">
-            Hydra 2<sup>nd</sup> Layer scaling for Extended UTXO
+            <a name="hydra">
+              Hydra 2<sup>nd</sup> Layer scaling for Extended UTXO
+            </a>
           </h3>
           <p class="content pl-2" />
           <p class="content pl-2" />
