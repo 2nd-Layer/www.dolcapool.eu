b273774288685f4e48a24fab0c1c1e2e367f6c3b
Cody Butz <codyjbutz@gmail.com>
2021-03-04 13:38:24 -0500

    Bug Fixes

--- src/frontend/components/header.vue
+++ src/frontend/components/header.vue
@@ -6,7 +6,12 @@
 </template>
 
 <script>
+import NavBarClone from "~/components/Navigation/NavBarClone";
+import NavBarStatic from "~/components/Navigation/NavBarStatic";
+
 export default {
   name: "App",
+
+  components: { NavBarClone, NavBarStatic }
 }
 </script>
